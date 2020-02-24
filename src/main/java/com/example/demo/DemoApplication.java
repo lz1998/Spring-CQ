9eb27c8be72c925b4c910116217084bdd6b6d5eb
lz1998 <875543533@qq.com>
2020-02-24 15:13:31 +0800

    config init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        Config.init();
         SpringApplication.run(DemoApplication.class, args);
     }
 
